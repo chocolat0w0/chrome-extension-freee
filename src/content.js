dcd11c13a8ff96c931e0bc73a36a722a04db673e
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2024-10-16 21:41:58 +0900

    freeeの勤怠画面に残業時間を表示

---
+++ src/content.js
@@ -0,0 +1,62 @@
+const waitForElement = (selector, timeout = 10000) => {
+  return new Promise((resolve, reject) => {
+    const observer = new MutationObserver((mutationsList, observer) => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        resolve();
+      }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    setTimeout(() => {
+      observer.disconnect();
+      reject(new Error("Element not found within timeout"));
+    }, timeout);
+  });
+};
+
+waitForElement(".employee-work-record-summary")
+  .then(() => {
+    const summaryItems = document.querySelectorAll(
+      ".employee-work-record-summary .item"
+    );
+
+    const dayCount = [...summaryItems]
+      .find((item) => {
+        return item.querySelector(".label").innerHTML.startsWith("労働日数");
+      })
+      .querySelector(".body")
+      .textContent.replace("日", "");
+
+    const totalWorkTimeElem = [...summaryItems]
+      .find((item) => {
+        return item.querySelector(".label").innerHTML.startsWith("総勤務時間");
+      })
+      .querySelector(".body");
+
+    const totalWorkHour = totalWorkTimeElem.querySelector(
+      ".hour-min__hour .hour-min__value"
+    ).textContent;
+    const totalWorkMinElem = totalWorkTimeElem.querySelector(
+      ".hour-min__min .hour-min__value"
+    );
+    const totalWorkMin = totalWorkMinElem ? totalWorkMinElem.textContent : 0;
+
+    const overtimeHour = totalWorkHour - dayCount * 8;
+
+    const overtimeElem = document.createElement("div");
+    overtimeElem.classList.add("item");
+    const overtimeLabel = document.createElement("div");
+    overtimeLabel.classList.add("label");
+    overtimeLabel.textContent = "だいたいの残業時間";
+    const overtimeValue = document.createElement("div");
+    overtimeValue.classList.add("body");
+    overtimeValue.textContent = `${overtimeHour} 時間 ${totalWorkMin} 分`;
+    overtimeElem.appendChild(overtimeLabel);
+    overtimeElem.appendChild(overtimeValue);
+    document.querySelector(".items.main-items").appendChild(overtimeElem);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
